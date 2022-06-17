6bc708c18c018a2eda381bdabfb03cf313cd81b7
Abhik Khanra <abhik.rk@gmail.com>
2022-06-17 22:56:04 +0530

    Trying to say hello to Canvaskit, from Cljs

---
+++ src/hello_canvaskit/app.cljs
@@ -0,0 +1,9 @@
+(ns hello-canvaskit.app)
+
+(defn init []
+  (println "Hello Canvaskit World!"))
+
+(comment
+  ;;Eval this form to see it show up in the browser
+  ;; console of the running app
+  (println "Let's go!"))
